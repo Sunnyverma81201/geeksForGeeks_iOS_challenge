c82eb942003222cb08a54688a563209d746b9cf0
Apoorv Verma <vermasunny224@gmail.com>
2023-06-26 11:54:29 +0530

    Initial Commit

---
+++ GFG_Challenge/GFG_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GFG_ChallengeApp.swift
+//  GFG_Challenge
+//
+//  Created by Apoorv Verma on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GFG_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
